f05c93751378db371ef20da5dc7c96afeb2dac9e
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-08-27 17:30:32 +0300

    stopwatch added

--- script.js
+++ script.js
@@ -7,9 +7,58 @@ function showTimeDate() {
     
     displayTime.innerHTML = time.toLocaleTimeString();
     displayDate.innerHTML = time.toLocaleDateString("en-GB", options);
-    
 }
 
 setInterval(() => {
     showTimeDate();
 }, 1000);
+
+
+let minute = 0;
+let second = 0;
+let splitSecond = 0;
+let isStopped = true;
+const displayTimer = document.getElementById("displayTimer");
+
+function startTimer() {
+    if (isStopped) {
+        isStopped = false;
+        timer();
+    }
+}
+
+function stopTimer() {
+    if (!isStopped) {
+        isStopped = true;
+    }
+}
+
+function timer() {
+    if (!isStopped) {
+        splitSecond = parseInt(splitSecond);
+        second = parseInt(second);
+        minute = parseInt(minute);
+
+        splitSecond++;
+        if (splitSecond == 100) {
+            second++;
+            splitSecond = 0;
+        }
+        if (second == 60) {
+            minute++;
+            second = 0;
+        }
+
+        displayTimer.innerHTML = `${minute} : ${second} : ${splitSecond}`;
+
+        setTimeout("timer()", 16.666666666667)
+    }
+}
+
+function resetTimer() {
+    displayTimer.innerHTML = "00 : 00 : 00";
+    splitSecond = 0;
+    second = 0;
+    minute = 0;
+    isStopped = true;
+}
